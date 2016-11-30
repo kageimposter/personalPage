2cb466b414ddd84cb55e23b113bcb2dcf86f4d4c
kageimposter <kageimposter@gmail.com>
2016-11-30 11:30:26 -0600

    this is all

--- sqlFolderPhpFolder/startpage.php
+++ sqlFolderPhpFolder/startpage.php
@@ -1,6 +1,6 @@
 <?php
 
-session_start();
+/*session_start();
 setcookie(session_name(),session_id(), time() + 2592000)
 ob_implicit_flush();
     if( function_exits('ob_get_level'))
@@ -21,10 +21,17 @@ if (get_magic_quotes_runtime()) {
 	set_magic_quotes_runtime(0);
 }
 
+*/
+
+$con = mysql_connect(localhost,browsePage,jackTheRipper);
+  mysql_select_db(fileServer,$con);
 
 
-
-
+$table = "ENTRIES";
+mysql_query = ("CREATE TABLE IF NOT EXISTS '$table' ( 'ID' INT NOT NULL AUTO_INCREMENT . PRIMARY KEY ('id'))");
+mysql_query = ("ALTER TABLE '$table' ADD 'TITLE' TEXT  NOT NULL");
+mysql_query = ("ALTER TABLE '$table' ADD 'SUMMARY' TEXT  NOT NULL");
+mysql_query = ("ALTER TABLE '$table' ADD 'CONTENT' TEXT  NOT NULL");
 
 
 ?>
